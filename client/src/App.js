6623fede871e2782b61bfbd6b5a32b56f5e51da6
LanChuHoang <hoanglan.ict@gmail.com>
2022-09-22 17:21:29 +0700

    remove admin part in client folder

--- client/src/App.js
+++ client/src/App.js
@@ -11,15 +11,6 @@ import Catalog from "./pages/Catalog";
 import Detail from "./pages/detail/Detail";
 import RequireAuth from "./components/auth/RequireAuth";
 import PersistLogin from "./components/auth/PersistLogin";
-import AdminLayout from "./components/layouts/AdminLayout";
-import Dashboard from "./pages/admin/dashboard/Dashboard";
-import Users from "./pages/admin/users/Users";
-import Movies from "./pages/admin/movies/Movies";
-import Shows from "./pages/admin/shows/Shows";
-import People from "./pages/admin/people/People";
-import UpsertMovie from "./pages/admin/upsert-movie/UpsertMovie";
-import UpsertShow from "./pages/admin/upsert-show/UpsertShow";
-import UpsertPerson from "./pages/admin/upsert-person/UpsertPerson";
 
 function App() {
   return (
@@ -31,34 +22,8 @@ function App() {
           <Route element={<MainLayout />}>
             <Route path="/" element={<Home />} />
             <Route path="/:itemType/:browseType" element={<Catalog />} />
+            <Route path="/:itemType/detail/:id" element={<Detail />} />
             <Route path="/:itemType/:id/detail" element={<Detail />} />
-          </Route>
-        </Route>
-
-        <Route element={<RequireAuth forAdmin={true} />}>
-          <Route path="/admin" element={<AdminLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="users" element={<Users />} />
-            <Route path="movies" element={<Movies />} />
-            <Route path="shows" element={<Shows />} />
-            <Route path="people" element={<People />} />
-
-            <Route path="user">
-              <Route index element={<p>Add user</p>} />
-              <Route path=":id" element={<p>Edit user</p>} />
-            </Route>
-            <Route path="movie">
-              <Route index element={<UpsertMovie />} />
-              <Route path=":id" element={<UpsertMovie />} />
-            </Route>
-            <Route path="show">
-              <Route index element={<UpsertShow />} />
-              <Route path=":id" element={<UpsertShow />} />
-            </Route>
-            <Route path="person">
-              <Route index element={<UpsertPerson />} />
-              <Route path=":id" element={<UpsertPerson />} />
-            </Route>
           </Route>
         </Route>
       </Route>
