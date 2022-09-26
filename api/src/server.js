6e9dfafbcacbd10d20bf44e55b65ec3b23c563c8
LanChuHoang <hoanglan.ict@gmail.com>
2022-09-26 11:21:51 +0700

    change install command

--- api/src/server.js
+++ api/src/server.js
@@ -3,7 +3,7 @@ const app = require("./app");
 const mongoService = require("./services/mongo.service");
 
 const server = http.createServer(app);
-const PORT = process.env.port || 8000;
+const PORT = process.env.PORT || 8000;
 
 mongoService.connect();
 
