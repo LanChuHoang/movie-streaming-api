57c5ba1041c138faad2a2c64311fa41aeb49c411
LanChuHoang <hoanglan.ict@gmail.com>
2022-09-26 11:54:21 +0700

    update client

--- client/src/api/backendApi/axiosClient.js
+++ client/src/api/backendApi/axiosClient.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: process.env.REACT_APP_BACKEND_BASE_URL,
   timeout: 2500,
 });
 
 export const axiosPrivateClient = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: process.env.REACT_APP_BACKEND_BASE_URL,
   timeout: 2500,
   withCredentials: true,
 });
