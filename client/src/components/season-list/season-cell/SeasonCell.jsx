d6b4bd96028faa9655a628f608165ea01801f628
LanChuHoang <hoanglan.ict@gmail.com>
2022-10-13 19:49:21 +0700

    fix season cell warnings

--- client/src/components/season-list/season-cell/SeasonCell.jsx
+++ client/src/components/season-list/season-cell/SeasonCell.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import PlayButton from "../../buttons/play-button/PlayButton";
 import FallbackImage from "../../fallback-image/FallbackImage";
 import Overlay from "../../overlay/Overlay";
