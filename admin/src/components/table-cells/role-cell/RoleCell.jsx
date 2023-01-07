e1aaad24f840cc7b6a4b29f5d8ef2d7e0961ca88
LanChuHoang <hoanglan.ict@gmail.com>
2023-01-07 14:44:32 +0700

    split repo to 3 projects

--- admin/src/components/table-cells/role-cell/RoleCell.jsx
+++
@@ -1,7 +0,0 @@
-import "./roleCell.scss";
-
-const RoleCell = ({ children, className }) => {
-  return <div className={`role-cell-root ${className}`}>{children}</div>;
-};
-
-export default RoleCell;
