06a02e768475bc535db3f60c73eae9ee9796fc78
Lan Chu <hoanglan.ict@gmail.com>
2023-03-30 04:26:27 +0700

    fix wrong admin client url

--- src/configs/app.config.js
+++ src/configs/app.config.js
@@ -3,7 +3,7 @@ const MAX_BODY_SIZE = "16mb";
 const WHITE_LIST = [
   "http://localhost:3000",
   "https://gr1-client.netlify.app",
-  "https://gr1-api.netlify.app",
+  "https://gr1-admin.netlify.app",
 ];
 
 const CORS_OPTIONS = {
