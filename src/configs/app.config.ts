e7cd0989f4e1405ff3b63e958c03fcd3a25d009e
Lan Chu <hoanglan.ict@gmail.com>
2023-08-08 00:53:11 +0700

    update

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -1,7 +1,12 @@
 export default () => ({
   port: parseInt(process.env.PORT || "") || 8000,
   cors: {
-    origin: "*",
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://gr1-client.netlify.app",
+      "https://gr1-api.netlify.app",
+    ],
     credentials: true,
   },
 });
